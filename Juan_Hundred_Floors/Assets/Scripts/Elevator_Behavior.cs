72956beeaaedf1de76ac7ca0243dc1203ed4319a
Brian Jackson <bvj23@Brians-MacBook-Pro-3.local>
2017-01-15 13:52:58 -0500

    Added elevator drop (semi coded)

---
+++ Juan_Hundred_Floors/Assets/Scripts/Elevator_Behavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator_Behavior : MonoBehaviour {
+
+	public float tVal;
+	public Transform target; // b = startPosition + offset
+	public AnimationCurve curve;
+	public float scale = 1f;
+	public float duration = 5f;
+	public Vector3 startPosition; // a
+	private Vector3 targetPosition;
+	public float elapsed = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("e")){
+			Drop ();
+		}
+	}
+
+	void Drop () {
+		tVal = elapsed / duration;
+		elapsed += Time.deltaTime;
+		targetPosition = target.position;
+		//		Vector3 actualPosition = startPosition + scale * tVal * (targetPosition - startPosition);// A + t * (B - A)
+		Vector3 actualPosition = Vector3.Lerp(startPosition, targetPosition, curve.Evaluate(tVal));
+		transform.position = actualPosition;
+	}
+}
---
+++ Juan_Hundred_Floors/Assets/Scripts/Tween_Mover_Easy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tween_Mover_Easy : MonoBehaviour {
+	[Range(0f,1f)]
+	public float tVal;
+	public Transform target; // b = startPosition + offset
+	public AnimationCurve curve;
+
+	public Color startColor, endColor;
+
+	private Material mat;
+
+	public float scale = 1f;
+
+	public float duration = 5f;
+
+	public Vector3 startPosition; // a
+	private Vector3 targetPosition;
+
+	public float elapsed = 0f;
+
+	void Start () {
+		mat = this.GetComponent<MeshRenderer>().material;
+	}
+
+
+	void Update () {
+		tVal = elapsed / duration;
+		elapsed += Time.deltaTime;
+		targetPosition = target.position;
+		//		Vector3 actualPosition = startPosition + scale * tVal * (targetPosition - startPosition);// A + t * (B - A)
+		Vector3 actualPosition = Vector3.Lerp(startPosition, targetPosition, curve.Evaluate(tVal));
+		mat.color = Color.Lerp(startColor, endColor, curve.Evaluate(tVal));
+		transform.position = actualPosition;
+	}
+}
