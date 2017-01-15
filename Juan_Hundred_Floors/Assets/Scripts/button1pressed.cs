8e491b5c9d2af414979254570db0edd253dd62d6
Jessica Nebolsky <jessican310@hotmail.com>
2017-01-15 13:50:18 -0500

    button1press

---
+++ Juan_Hundred_Floors/Assets/Scripts/button1pressed.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class button1pressed : MonoBehaviour {
+	GameObject elevatorLight;
+
+	float randomThreshold;
+	float minThreshold = 0.5f; // min time between flashes
+	float maxThreshold = 3.0f; // max time between flashes
+
+	float timer; //don't touch
+	bool isFlashing; //don't touch
+
+	int randomFlashes; //don't touch
+	int minFlashes = 3; //min number of flashes
+	int maxFlashes = 5; //max number of flashes (max is exclusive)
+
+	float timeBetweenFlashes = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFlashing == false) {
+			timer += Time.deltaTime;
+		}
+	}
+
+	//when mouse clicks the button
+	void OnMouseDown(){
+		//turn the lights on and off
+		//set thresholds and ranges
+		randomThreshold = Random.Range(minThreshold, maxThreshold);
+		randomFlashes = Random.Range(minFlashes, maxFlashes);
+
+		if (timer > randomThreshold) {
+			timer = 0.0f;
+			StartCoroutine(FlashingLights());
+		}
+
+	}
+
+	IEnumerator FlashingLights(){
+		isFlashing = true;
+
+		for (int x = 0; x < randomFlashes; x++) {
+			elevatorLight.GetComponent<Light>().enabled = false;
+			yield return new WaitForSeconds(timeBetweenFlashes);
+			elevatorLight.GetComponent<Light>().enabled = true;
+			yield return new WaitForSeconds(timeBetweenFlashes);
+		}
+
+		//reset variables for the next flashing section
+		randomThreshold = Random.Range(minThreshold, maxThreshold);
+		randomFlashes = Random.Range(minFlashes, maxFlashes);
+
+		isFlashing = false;
+	}
+}
