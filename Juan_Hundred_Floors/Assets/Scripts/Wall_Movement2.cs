40d4aae5c3b5794e09b89c281d2b2d755f95be6c
Brian Jackson <bvj23@n3-47-42.dhcp.drexel.edu>
2017-01-17 13:08:09 -0500

    Finished product

--- Juan_Hundred_Floors/Assets/Scripts/Wall_Movement2.cs
+++ Juan_Hundred_Floors/Assets/Scripts/Wall_Movement2.cs
@@ -15,10 +15,21 @@ public class Wall_Movement2 : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		//THIS IS SOME MOTHERFUCKING DEBUG CODE YOU BITCHES
+		//THIS IS ALSO THE *BETTER* VERSION OF OnMouseDown
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				Debug.Log ("Name = " + hit.collider.name);
+				Debug.Log ("Tag = " + hit.collider.tag);
+				Debug.Log ("------------------");
+			}
+		}
 		if (button1.GetComponent<button1pressed>().isFlashing == false) {
 			transform.localPosition = Vector3.Lerp (transform.localPosition, endPosition, Time.deltaTime * speed);
 		} else {
-			transform.localPosition = new Vector3(-0.3670709f, 1.07f, -0.8533344f);
+			transform.localPosition = Vector3.Lerp (transform.localPosition, new Vector3(-0.3670709f, 1.07f, -0.8533344f) , Time.deltaTime * speed);
 		}
 	}
 }
--- Juan_Hundred_Floors/Assets/Scripts/button2pressed.cs
+++ Juan_Hundred_Floors/Assets/Scripts/button2pressed.cs
@@ -3,10 +3,48 @@ using System.Collections;
 
 public class button2pressed : MonoBehaviour {
 	public GameObject Elevator;
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	public Transform camTransform;
+	public GameObject playerCam;
+
+	// How long the object should shake for.
+	public float shakeDuration; 
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.7f;
+	public float decreaseFactor = 1.0f;
+
+	Vector3 originalPos;
+	public AudioClip Rumble;
+	private AudioSource audio;
+
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
 
 	// Use this for initialization
 	void OnMouseDown() {
-		Elevator.GetComponent<Elevator_Behavior>().Drop();
+		shakeDuration = 20f;
+		Camera camera = Camera.main;
+		originalPos = camTransform.localPosition;
+		audio.PlayOneShot (Rumble);
+	}
+
+	void Update()
+	{
+		if (shakeDuration > 0)
+		{
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+			shakeDuration = 0f;
+		}
+		else
+		{
+			shakeDuration = 0f;
+			camTransform.localPosition = originalPos;
+		}
 	}
 
 }
